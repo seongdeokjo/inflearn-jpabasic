92e3cbf80bd60ce9eaf1a5eb542d81e719f11660
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-24 20:55:05 +0900

    feat: inflearn - spring jpa basic : hello jpa - 영속성 컨텍스트2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Collections;
 import java.util.List;
 
 public class JpaMain {
@@ -15,24 +16,12 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            Member findMember = em.find(Member.class, 1L);
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(1)
-//                    .setMaxResults(10)
-//                    .getResultList();
-//            for (Member member : result) {
-//                System.out.println("member.getName() = " + member.getName());
-//            }
-            // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloAB");
 
             // 영속
-            System.out.println("==== before ====");
-            em.persist(member);
-            em.detach(member);
-            System.out.println("==== after ====");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+
+            System.out.println("==========================");
 
             tx.commit();
         } catch (Exception e) {
