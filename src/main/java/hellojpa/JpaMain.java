c44a4b67e21457b203c3b60205a87d2785c4e273
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-24 20:24:42 +0900

    feat: inflearn - spring jpa basic : hello jpa - 영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,13 +16,23 @@ public class JpaMain {
         tx.begin();
         try {
 //            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloAB");
+
+            // 영속
+            System.out.println("==== before ====");
+            em.persist(member);
+            em.detach(member);
+            System.out.println("==== after ====");
 
             tx.commit();
         } catch (Exception e) {
