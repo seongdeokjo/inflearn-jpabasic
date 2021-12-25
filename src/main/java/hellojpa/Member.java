f020ad984765ddb4b3981695fa0bdec0d9b1c7b7
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-25 18:19:09 +0900

    feat: inflearn - spring jpa basic : hello jpa - 기본 키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,25 +4,39 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+//2. seq 사용 ->
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
+
+//3. table seq 사용 ->@TableGenerator(name = "MEMBER_SEQ_GENERATOR",table = "MY_SEQUENCES",pkColumnValue = "MEMBER_SEQ",allocationSize = 1)
 public class Member {
 
     @Id
+//  1. @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  2. @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+//  3. @GeneratedValue(strategy = GenerationType.TABLE,generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
+
     @Column(name = "name")
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public Member() {
     }
-
-
 
 }
