0bbe1073170079ce43472911164a3fe433927424
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-28 17:25:36 +0900

    feat: inflearn - spring jpa basic : hello jpa - 프록시와 연관관계 관리 - 영속성 전이(cascade)와 고아 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,24 +13,37 @@ import java.util.List;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
-
-            member.setTeam(team);
-
+//          ------------------------------------------------------------------
+//          -cascade- 소유자가 1개일 경우에만 사용
+            Child child1 = new Child();
+            Child child2 = new Child();
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+            em.persist(parent);
             em.flush();
             em.clear();
+//          고아 객체 - 참조하는 곳이 하나일 때 사용해야함, 특정 엔티티가 개인 소유할 때 사용
+//          oorphanRemoval = true 사용
+            Parent findParent = em.find(Parent.class, parent.getId());
+//            em.remove(findParent);
+            findParent.getChildList().remove(0);
+//            em.persist(child1);
+//            em.persist(child2);
+//          -----------------------------------------------------------------
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            em.persist(member);
+//            member.setTeam(team);
+//            em.flush();
+//            em.clear();
 //           --------------------------------------------------------------------------------
 //           지연 로딩 - lazy
 //            Member m = em.find(Member.class, member.getId());
@@ -41,11 +54,11 @@ public class JpaMain {
 //          --------------------------------------------------------------------------------
 //          즉시 로딩 - eager 
 //          ** 가급적 지연 로딩만 사용하자
-            Member m = em.find(Member.class, member.getId());
-            System.out.println("m = " + m.getTeam().getClass());
-            System.out.println("============");
-            System.out.println("teamName = "+m.getTeam().getName());    // 실제 team 엔티티
-            System.out.println("============");
+//            Member m = em.find(Member.class, member.getId());
+//            System.out.println("m = " + m.getTeam().getClass());
+//            System.out.println("============");
+//            System.out.println("teamName = "+m.getTeam().getName());    // 실제 team 엔티티
+//            System.out.println("============");
 
 //          ----------------------------------------------------------------------------------
 //           프록시
